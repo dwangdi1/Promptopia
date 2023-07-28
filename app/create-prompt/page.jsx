98b9fc69fed555fd52c6a1aaf4670e10c476a523
dwangdi1 <75458457+dwangdi1@users.noreply.github.com>
2023-07-28 12:32:04 -0700

    Post Route
    POST route created for prompts

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,12 +1,15 @@
 "use client"
 
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import Form from '@components/Form';
 
 
 const CreatePrompt = () => {
+    const router = useRouter();
+    const {data: session} = useSession();
+
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
